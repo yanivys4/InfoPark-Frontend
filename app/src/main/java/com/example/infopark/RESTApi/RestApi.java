3807c1d088bd48403ec29c8069831ee022fe85d1
yanivys4 <yanivys4@gmail.com>
2021-01-26 13:36:04 +0200

    /

--- app/src/main/java/com/example/infopark/RESTApi/RestApi.java
+++ app/src/main/java/com/example/infopark/RESTApi/RestApi.java
@@ -1,10 +1,7 @@
 package com.example.infopark.RESTApi;
 
-import com.example.infopark.Utils.Test;
-
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface RestApi {
