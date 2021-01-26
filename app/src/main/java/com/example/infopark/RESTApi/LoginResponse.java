541b5e8c47f01562bf608854387e4d1812a78d03
yanivys4 <yanivys4@gmail.com>
2021-01-26 13:09:39 +0200

    make the class derive from ResponseMessage and add String field for email

---
+++ app/src/main/java/com/example/infopark/RESTApi/LoginResponse.java
@@ -0,0 +1,9 @@
+package com.example.infopark.RESTApi;
+
+public class LoginResponse extends ResponseMessage {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+}
