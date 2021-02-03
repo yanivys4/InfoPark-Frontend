44d0190e401e2ace76c3b7f819940112a715c866
DvirDatner <dat.dvi@gmail.com>
2021-02-03 13:43:52 +0200

    added latlong to ReportForm

--- app/src/main/java/com/example/infopark/RESTApi/ReportForm.java
+++ app/src/main/java/com/example/infopark/RESTApi/ReportForm.java
@@ -13,12 +13,15 @@ public class ReportForm {
 
     private final String maxHours;
 
-    public ReportForm(String user, String fromA, String toA, String fromB, String toB, String maxHours) {
+    private final LatitudeLongitude latitudeLongitude;
+
+    public ReportForm(String user, String fromA, String toA, String fromB, String toB, String maxHours, LatitudeLongitude latitudeLongitude) {
         this.user = user;
         this.fromA = fromA;
         this.toA = toA;
         this.fromB = fromB;
         this.tob = toB;
         this.maxHours = maxHours;
+        this.latitudeLongitude = latitudeLongitude;
     }
 }
