f42980a167efeda935d22589daad08e1de26c245
yanivys4 <yanivys4@gmail.com>
2021-02-11 12:04:53 +0200

    /

--- app/src/main/java/com/example/infopark/Utils/Utils.java
+++ app/src/main/java/com/example/infopark/Utils/Utils.java
@@ -1,17 +1,22 @@
 package com.example.infopark.Utils;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.infopark.R;
 
+/**
+ * This class is utils class that serves all the other project classes.
+ */
 public final class Utils {
 
-    
+    /**
+     * This function shows a toast to the user with the given message.
+     * @param context the context to show the toast to.
+     * @param message the message to display.
+     */
     public static void showToast(Context context, String message){
         Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
         View view = toast.getView();
