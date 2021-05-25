51244e727b0c3500f3dc14088bb26190b33a4f3a
yanivys4 <yanivys4@gmail.com>
2021-05-25 14:04:49 +0300

    re commit with git ignore

--- app/src/main/java/com/example/infopark/RESTApi/LoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.infopark.RESTApi;
-
-public class LoginResponse extends ResponseMessage {
-    private String uniqueID;
-
-    public String getUniqueID() {
-        return uniqueID;
-    }
-}
